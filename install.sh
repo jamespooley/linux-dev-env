6e2d3ae4070e3351cba12ef94a3b348735474e26
James Pooley <james.pooley@gmail.com>
2020-03-10 21:17:34 -0400

    Add initial install script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+set -euf -o pipefail
+
+function run_dots() {
+
+  sudo -v
+
+  while true; do sudo -n true; sleep 60; kiill -0 "$$" || exit; done 2/dev/null &
+
+  for args in "$@"; do
+    if [ $arg == "bootstrap" ] || [ $arg == "all" ]; then
+      echo ""
+      echo "Syncing linux-dev-environment repo to local machine..."
+      echo ""
+      ./bootstrap.sh
+    fi
+    if [ $arg == "python" ] || [ $arg == "all" ]; then
+      echo ""
+      echo "Setting up Python development environments..."
+      echo ""
+      ./python.sh
+    fi
+    if [ $arg == "r" ] || [ $arg == "all" ]; then
+      echo ""
+      echo "Setting up R development environment..."
+      echo ""
+      ./r.sh
+    fi
+    if [ $arg == "postgres" ] || [ $arg == "all" ]; then
+      echo ""
+      echo "Setting up PostgreSQL..."
+      echo ""
+      ./postgres.sh
+    fi
+  done
+}
+
+
+if [[ $reply =~ ^[Yy]$ ]]; then
+  run_dots $@
+fi
+
+unset run_dots

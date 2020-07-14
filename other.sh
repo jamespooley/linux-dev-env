7da6c901d6d3fee8eb5cbfdd83cec7c05832475c
James Pooley <james.pooley@gmail.com>
2020-07-13 21:24:58 -0400

    Install some Kubernetets command line utils

--- other.sh
+++ other.sh
@@ -64,3 +64,15 @@ apt-get update && sudo apt-get install git-secret
 apt-get install unixodbc unixodbc-dev --install suggests
 apt-get install odbc-postgresql
 apt-get install libsqliteodbc
+
+
+# Kubernetes Things
+
+curl --silent --location \
+  "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | \
+  tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
